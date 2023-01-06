472e7ebe09dbea26e96443a479835609e138587d
hemddabral <49645129+hemddabral@users.noreply.github.com>
2023-01-06 12:40:54 +0530

    handle corner case

--- cpp/leetcode/top_interview_questions/search_substring.cpp
+++ cpp/leetcode/top_interview_questions/search_substring.cpp
@@ -1,9 +1,11 @@
 // https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/solutions/
-
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        for(int i=0; i<haystack.size()-needle.size(); i++) {
+        if (needle.size() > haystack.size())
+            return -1;
+            
+        for(int i=0; i<=haystack.size()-needle.size(); i++) {
             if (haystack.substr(i, needle.size()) == needle) {
                 return i;
             }
