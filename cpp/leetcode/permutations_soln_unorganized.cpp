75aaec35ce33f9a33d28f140384ff736e6fe08ad
hemddabral <49645129+hemddabral@users.noreply.github.com>
2023-01-18 09:17:59 +0530

    simple solution to permutations problem

--- cpp/leetcode/permutations_soln_unorganized.cpp
+++ cpp/leetcode/permutations_soln_unorganized.cpp
@@ -1,59 +1,34 @@
 // https://leetcode.com/problems/permutations/
 
-struct Vec_Equal {
-    public:
-        bool operator()(const vector<int>& a, const vector<int>& b) const {
-        if (a.size() != b.size())
-            return false;
-
-        for(int i=0; i<a.size(); i++) {
-            if (a[i] == b[i])  
-                continue;
-            else
-                return false;
-        }
-        
-        return true;
-    }
-
-};
-
-struct Vec_Hasher {
-    public:
-    size_t operator()(const vector<int>& vi) {
-
-        size_t h = 0;
-        for(auto i : vi) {
-            h ^= hash<int>()(i);
-        }
-        return h;
-
-    }
-};
-
 class Solution {
-public:
-    vector<vector<int>> permute(vector<int>& nums) {
-        unordered_set<vector<int>, Vec_Hasher, Vec_Equal> s;
-        vector<vector<int>> ret;
-        if(!nums.size()) return ret;
-        vector<int> seed;
-        seed.push_back(nums[0]);
-        s.insert(seed);
-        for(int i=1; i<nums.size(); i++) {
-            unordered_set<vector<int>, Vec_Hasher, Vec_Equal> s2;
-            for(auto sub : s) {  // for every sub-array in s
-                for(auto itr=sub.begin(); itr!=sub.end(); itr++) {
-                    sub.insert(itr, nums[i]);
-                    s2.insert(sub);
-                }
-                s = s2;                
+    vector<int> remove_element(int mask, const vector<int>& nums) {
+        vector<int> ret;
+        for(int i=0; i<nums.size(); i++) {
+            if(i != mask) {
+                ret.push_back(nums[i]);
             }
         }
+        return ret;
+    }
+    
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> vv;
+        if(nums.size()==1) {
+            vv.push_back(nums);
+            return vv;
+        }
 
-        for(auto sub : s)
-            ret.push_back(sub);           
+        for(int i=0; i<nums.size(); i++) {
+            vector<int> v1 = remove_element(i, nums);
+            auto vv1 = permute(v1);
+            for( auto &x : vv1)
+                x.emplace_back(nums[i]);
+            for(auto y : vv1)
+                vv.emplace_back(y);
+        }
+        return vv;
+            
         
-        return ret;   
     }
 };
