c18e394b8756cc40b7e43acf84e5e523c2bcd6c6
Hem Dutt Dabral <hem.dabral.office@gmail.com>
2023-01-19 22:21:35 +0530

    added binary algo and uts

---
+++ tdd_googletest/algo_library/src/permutuations.cpp
@@ -0,0 +1,71 @@
+/*
+ * permutuations.cpp
+ *
+ *  Created on: 17-Jan-2023
+ *      Author: hem
+ */
+
+#include <functional>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+
+struct Vec_Equal {
+    public:
+        bool operator()(const vector<int>& a, const vector<int>& b) const {
+        if (a.size() != b.size())
+            return false;
+
+        for(int i=0; i<a.size(); i++) {
+            if (a[i] == b[i])
+                continue;
+            else
+                return false;
+        }
+
+        return true;
+    }
+
+};
+
+struct Vec_Hasher {
+    public:
+    size_t operator()(const vector<int>& vi) const {
+
+        size_t h = 0;
+        for(auto i : vi) {
+            h ^= hash<int>()(i);
+        }
+        return h;
+
+    }
+};
+
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        unordered_set<vector<int>, Vec_Hasher, Vec_Equal> s;
+        vector<vector<int>> ret;
+        if(!nums.size()) return ret;
+        vector<int> seed;
+        seed.push_back(nums[0]);
+        s.insert(seed);
+        for(int i=1; i<nums.size(); i++) {
+            unordered_set<vector<int>, Vec_Hasher, Vec_Equal> s2;
+            for(auto sub : s) {  // for every sub-array in s
+                for(auto itr=sub.begin(); itr!=sub.end(); itr++) {
+                    sub.insert(itr, nums[i]);
+                    s2.insert(sub);
+                }
+                s = s2;
+            }
+        }
+
+        for(auto sub : s)
+            ret.push_back(sub);
+
+        return ret;
+    }
+};
--- tdd_googletest/unit_tests/src/test.cpp
+++ tdd_googletest/unit_tests/src/test.cpp
@@ -1,4 +1,5 @@
 #include "math_functions.h"
+#include "binary_search.h"
 #include <gtest/gtest.h>
 #include <iostream>
 
@@ -7,3 +8,26 @@ using namespace std;
 TEST(TestSuiteMath, Factorial_TEST) {
 	ASSERT_TRUE(fact(0)==1);
 }
+
+TEST(TestSuite_Binary_Search, T1) {
+	vector<int> nums {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	ASSERT_EQ(binary_search(nums, 0), 0);
+	ASSERT_EQ(binary_search(nums, 1), 1);
+	ASSERT_EQ(binary_search(nums, 5), 5);
+	ASSERT_EQ(binary_search(nums, 2), 2);
+	ASSERT_EQ(binary_search(nums, 8), 8);
+	ASSERT_EQ(binary_search(nums, 9), 9);
+
+}
+
+TEST(TestSuite_Binary_Search, T2) {
+	vector<int> nums {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	ASSERT_EQ(binary_search(nums, 10), -1);
+	ASSERT_EQ(binary_search(nums, 11), -1);
+	ASSERT_EQ(binary_search(nums, 15), -1);
+	ASSERT_EQ(binary_search(nums, 12), -1);
+	ASSERT_EQ(binary_search(nums, 18), -1);
+	ASSERT_EQ(binary_search(nums, 19), -1);
+
+}
+
