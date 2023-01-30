5c4555408469eead3c64d4ac39608bd8c41fbf76
Hem-dutt DABRAL <hem.dabral@st.com>
2023-01-30 16:53:27 +0530

    moved leetcode to top folder

--- cpp/linked_list/linked_list.cpp
+++ cpp/linked_list/linked_list.cpp
@@ -1,9 +1,37 @@
+// TODO: This code has bugs
 #include <iostream>
 #include <list>
 
-void reverse_list(list<int>& L) {
+using namespace std;
+
+list<int> reverse_list(list<int>& L) {
     list<int> l1;
-    for(const auto& node : list) {
-        list.pop_front();
+    for(const auto& node : L) {
+        l1.push_back(L.front());
+        L.pop_front();
     }
+    return l1;
+}
+
+ostream& operator<<(ostream& os, const list<int>& l1) {
+    for(int val : l1) {
+        os << val << "->";
+    }
+    os << endl;
+    return os;
+}
+
+int main() {
+    list<int> l {1,2,3,4};
+    cout << "demostrating how to invert a linked list" << endl;
+    for(auto x : l)
+        cout << x << "->";
+    cout << endl;
+    cout << l;
+    cout << "after reversing list: " << endl;
+    cout << reverse_list(l);
+    cout << "original:" << endl;
+    cout << l;
+
+    return 0;
 }
